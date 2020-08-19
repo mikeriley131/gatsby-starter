5a85e035bafdd51a299f842b37f2f5e6075b141d
Mike Riley <mikeriley131@gmail.com>
2020-08-19 00:14:25 -0600

    update image component

--- src/components/patterns/image.js
+++ src/components/patterns/image.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Image = ({ srcSmall, srcMed, srcLarge, alt }) => (
+export const Image = ({ srcSmall, srcMed, srcLarge, alt, lazyLoad }) => (
   <picture>
     <source srcSet={srcLarge} media="(min-width: 1200px)" />
     <source srcSet={srcMed} media="(min-width: 720px)" />
-    <img src={srcSmall} alt={alt} loading="lazy" />
+    <img src={srcSmall} alt={alt} loading={`${lazyLoad ? 'lazy' : 'eager'}`} />
   </picture>
 );
 
@@ -14,9 +14,11 @@ Image.propTypes = {
   srcMed: PropTypes.string,
   srcLarge: PropTypes.string,
   alt: PropTypes.string.isRequired,
+  lazyLoad: PropTypes.bool,
 };
 
 Image.defaultProps = {
+  lazyLoad: false,
   srcMed: '',
   srcLarge: '',
 };
